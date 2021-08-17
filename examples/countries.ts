e1767b2472847c7d04a79d4cf69c88e6273e52e7
Rage Brahimi <github@rage.al>
2021-08-17 13:59:37 +0100

    v1.2.0

--- examples/countries.ts
+++ examples/countries.ts
@@ -5,7 +5,7 @@ const SDK = new APIFutbol('token');
 const countries = async () => {
   try {
     const { data } = await SDK.countries.getCountries();
-    
+
     for (const country of data.countries) {
       console.log(country.id);
     }
